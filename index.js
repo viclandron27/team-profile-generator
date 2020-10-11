855249a9aaf6c253e65e08d39beb351f8fc0ba6c
Victoria Landron <viclandron@gmail.com>
2020-10-11 17:04:41 -0400

    running the application

--- index.js
+++ index.js
@@ -3,31 +3,89 @@ const Manager = require('./lib/Manger');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const fs = require('fs');
+const inquirer = require('inquirer');
 
 function Index() {
     this.employee;
+    this.employeeId;
+    this.employeeEmail;
 }
 
 Index.prototype.getUserName = function () {
     inquirer
-    .prompt({
-        type: 'text',
-        name: 'name',
-        message: "What is your team manager's name?",
-        validate: employeeInput => {
-            if (employeeInput) {
-                return true;
+        .prompt({
+            type: 'text',
+            name: 'name',
+            message: "What is your team manager's name?",
+            validate: employeeInput => {
+                if (employeeInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter the name of your team manager!');
+                    return false;
+                }
             }
-            else {
-                console.log('Please enter the name of your team manager!');
-                return false;
-            }
-        }
     })
     //destructre name from prompt object
     .then(({ name }) => {
         this.employee = new Employee(name);
+
+        this.getIdNum();
     });
 
-    console.log(Employee);
+
 };
+
+Index.prototype.getIdNum() = function () {
+    inquirer
+        .prompt({
+            type: 'text',
+            name: 'id',
+            message: "What is your team manager's employee ID?",
+            validate: employeeInput => {
+                if (employeeInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter the employee ID of your team manager!');
+                    return false;
+                }
+            }
+        })
+        .then(({ id }) => {
+            this.employeeId = new Employee(id);
+
+            this.getEmail();
+        });
+
+        
+}
+
+Index.prototype.getEmail() = function () {
+    inquirer
+        .prompt({
+            type: 'text',
+            name: 'email',
+            message: "What is your team manager's email?",
+            validate: employeeInput => {
+                if (employeeInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter the email of your team manager!');
+                    return false;
+                }
+            }
+        })
+        .then(({ email }) => {
+            this.this.employeeEmail = new Employee(email);
+            console.log(this.email)
+        });
+
+
+}
+
+module.exports = Index;
+
+new Index().getUserName();
